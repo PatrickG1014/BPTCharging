b9f8d55e7115c2536a68d85b713cf88aebe3c2a0
Gao <gaoheyang1014@gmail.com>
2022-04-07 06:08:57 +0800

    add server

---
+++ server/hello.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	trippb "bptcharging/proto/gen/go"
+	"encoding/json"
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	trip := trippb.Trip{
+		Start:       "abc",
+		End:         "def",
+		DurationSec: 3600,
+		FeeCent:     10000,
+		StartPos: &trippb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:  35,
+			Longitude: 115,
+		},
+		PathLocations: []*trippb.Location{
+			{
+				Latitude:  31,
+				Longitude: 119,
+			},
+			{
+				Latitude:  32,
+				Longitude: 118,
+			},
+		},
+		Status: trippb.TripStatus_IN_PROGRESS,
+	}
+	fmt.Println(&trip)
+	b, err := proto.Marshal(&trip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%X\n", b)
+
+	var trip2 trippb.Trip
+	err = proto.Unmarshal(b, &trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip2)
+
+	b, err = json.Marshal(&trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
+}
